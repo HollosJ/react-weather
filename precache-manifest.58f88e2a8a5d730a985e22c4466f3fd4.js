4b7fd5522a214e9d9e2b9673ec2f61e869d94bd3
HollosJ <hollosj99@gmail.com>
2020-06-28 14:02:03 +0100

    Updates

---
+++ precache-manifest.58f88e2a8a5d730a985e22c4466f3fd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f99ca714ceef463636f40f2db35fe7e",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "62628af23c9dae8e4e47",
+    "url": "/react-weather/static/css/main.934e3d02.chunk.css"
+  },
+  {
+    "revision": "ec7513f95a530bf57099",
+    "url": "/react-weather/static/js/2.3d60a562.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather/static/js/2.3d60a562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62628af23c9dae8e4e47",
+    "url": "/react-weather/static/js/main.0e3bc52b.chunk.js"
+  },
+  {
+    "revision": "f638013bd88fa5b26bac",
+    "url": "/react-weather/static/js/runtime-main.b9e6359b.js"
+  }
+]);
